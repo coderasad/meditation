acfdcedcab154ad8beaee6d5ccef84d3661d50b3
Foysal6862 <61239971+Foysal6862@users.noreply.github.com>
2021-11-21 21:11:38 +0600

    event details content design fix

--- resources/views/frontend/pages/event-details/section/event-detais-content.blade.php
+++ resources/views/frontend/pages/event-details/section/event-detais-content.blade.php
@@ -3,10 +3,6 @@
         <div class="row g-5">
             <div class="col-xl-4 col-lg-5">
                 <div class="events-details-text">
-                    <ul class="d-flex align-items-center ed-ul-1">
-                        <li><a href="javascript:void(0)" class="tc ps-700 cff s14">Live Now</a></li>
-                        <li class="ps-400 c85 s12">1.7k Watching</li>
-                    </ul>
                     <ul class="d-flex align-items-center ed-ul-2">
                         <li>
                             <hr>
@@ -15,10 +11,10 @@
                     </ul>
                     <h2 class="ps-700 c00 s32">Abandon he myself <br> and with this either fur</h2>
                     <h3 class="ps-700 c82 s22">With {{ $data->title }}</h3>
-                    <p class="ps-400 s16 c60">
-                        {{ $data->description }}
-                    </p>
-                    <a href="javascript:void(0)" class="secondary-btn">Watch Now</a>
+                    <p class="date-content">Date: 01-02-2021</p>
+                    <p class="contribution-fee-content">Contribution Fee: 10,000BDT </p>
+                    <p class="ps-400 s16 c60"> {{ $data->description }}</p>
+                    <a href="javascript:void(0)" class="secondary-btn join-btn">Join</a>
                 </div>
             </div>
             <div class="col-xl-8 col-lg-7">
